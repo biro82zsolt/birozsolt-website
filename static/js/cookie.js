0d898e23418573345bf48097835d7fc757f9bc26
biro82zsolt <birozsolt.pszi@gmail.com>
2025-07-21 22:08:55 +0200

    adatvedelem

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -3,7 +3,7 @@ window.onload = function () {
         const banner = document.createElement('div');
         banner.innerHTML = `
         <div style="position:fixed;bottom:0;width:100%;background:#222;color:#fff;padding:1rem;text-align:center;z-index:9999;">
-            Ez a weboldal sütiket használ. <a href="/static/adatvedelem" style="color:#0d6efd;">További info</a>
+            Ez a weboldal sütiket használ. <a href="/adatvedelem" style="color:#0d6efd;">További info</a>
             <button style="margin-left:1rem;" onclick="this.parentElement.style.display='none'; localStorage.setItem('cookieConsent', true);" class="btn btn-sm btn-primary">Értettem</button>
         </div>`;
         document.body.appendChild(banner);
